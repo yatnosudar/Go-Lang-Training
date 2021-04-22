93def168eaa63e3678ee48e58e9440b8ed39e095
gilangramadhanXIRPL2 <79070780+gilangramadhanXIRPL2@users.noreply.github.com>
2021-04-22 16:12:59 +0700

    Add files via upload

---
+++ gilangramadhan-Day1-belajar-golang-dasar/constant.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		    firstname string = "gilang"
+	 		lastname = "ramadhan"
+ 			value = 1000
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+	fmt.println(value)
+
+}
---
+++ gilangramadhan-Day1-belajar-golang-dasar/type-declarations.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	type NoKTP string
+	type married bool
+
+	var noKtpGilang NoKTP =1212121212
+	var mariedstatus married = true
+	fmt.println(noKtpGilang)
+	fmt.println(mariedstatus)
+
+}
